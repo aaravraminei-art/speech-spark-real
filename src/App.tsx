0ed51e65daddec8bac9bb41d347cca852e98931b
aaravraminei-art <aarav.raminei@manabadi.siliconandhra.org>
2026-06-27 13:28:16 -0400

    Integrate Google OAuth provider into App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { GoogleOAuthProvider } from "@react-oauth/google";
 import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
@@ -15,25 +16,28 @@ import ScrollReveal from "./components/ScrollReveal";
 const queryClient = new QueryClient();
 
 const App = () => (
-  <QueryClientProvider client={queryClient}>
-    <TooltipProvider>
-      <Toaster />
-      <Sonner />
-      <BrowserRouter>
-        <ScrollReveal />
-        <Routes>
-          <Route path="/" element={<Index />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/officers" element={<Officers />} />
-          <Route path="/events" element={<Events />} />
-          <Route path="/chapters" element={<Chapters />} />
-          <Route path="/faq" element={<FAQ />} />
-          {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </BrowserRouter>
-    </TooltipProvider>
-  </QueryClientProvider>
+  // 🔑 Replace the client ID string below with your actual Google Client ID
+  <GoogleOAuthProvider clientId="977275513345-gh1torrbsjfv7bhucakqnqbuclgshtut.apps.googleusercontent.com">
+    <QueryClientProvider client={queryClient}>
+      <TooltipProvider>
+        <Toaster />
+        <Sonner />
+        <BrowserRouter>
+          <ScrollReveal />
+          <Routes>
+            <Route path="/" element={<Index />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/officers" element={<Officers />} />
+            <Route path="/events" element={<Events />} />
+            <Route path="/chapters" element={<Chapters />} />
+            <Route path="/faq" element={<FAQ />} />
+            {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </BrowserRouter>
+      </TooltipProvider>
+    </QueryClientProvider>
+  </GoogleOAuthProvider>
 );
 
 export default App;
